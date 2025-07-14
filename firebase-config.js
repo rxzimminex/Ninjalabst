5358cb10f0be01dd3354f2b05fad6b8123a59c27
rxzimminex <eulucas971@gmail.com>
2025-07-14 09:36:44 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Substitua com suas credenciais do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCOA93Cj8m7m-XWxPpTV0VKNGr3UZdJUuc",
+  authDomain: "ninjalabs-9b54c.firebaseapp.com",
+  projectId: "ninjalabs-9b54c",
+  storageBucket: "ninjalabs-9b54c.firebasestorage.app",
+  messagingSenderId: "593775657030",
+  appId: "1:593775657030:web:9c4ef173bcb8b288398a47"
+};
+
+firebase.initializeApp(firebaseConfig);
